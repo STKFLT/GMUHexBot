786e5b2cf1dc8abcd55a464af2622f5a80205646
Alan Li <screensaver7@gmail.com>
2015-10-30 13:56:09 -0400

    Read from user keyboard

--- Comms/HexBotBrainComms/HexBotBrainComms.ino
+++ Comms/HexBotBrainComms/HexBotBrainComms.ino
@@ -1,14 +1,31 @@
 #include <Wire.h>
 
 void setup() {
+  Serial.begin(9600);
   Wire.begin(); // join i2c bus (address optional for master)
 }
 
 byte x = 0;
 
 void loop() {
+  
+  char cmd[3];
+  if (Serial.available() > 0) {
+    // read incoming serial data:
+    for (int i = 0; i < 3; i++){
+      char inChar = Serial.read();
+      cmd[i] = inChar;
+    }
+  }
+  
   Wire.beginTransmission(8); // transmit to device #8
-  Wire.write("RBT RUN LFT END");        // sends a number of bytes
+  if (cmd[0] != 0){
+    for (int i = 0; i < 3; i++){
+      Serial.print(cmd[i]);
+    }
+    Serial.println();
+  }
+  Wire.write(cmd);        // sends a number of bytes
   Wire.write(x);              // sends one byte
   Wire.endTransmission();    // stop transmitting
   
