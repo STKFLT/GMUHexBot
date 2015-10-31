6c4db63a1986b9cbc0e5c1202b87e7f399fe66fd
Zachary Ferguson <zfergus2@gmu.edu>
2015-10-31 00:44:54 -0400

    Updated the comms file in the hexBotWalk folder.

--- hexBotWalk/HexBotLegsComms.ino
+++ hexBotWalk/HexBotLegsComms.ino
@@ -3,11 +3,16 @@
  * Slave communication protocol for the HexBot
  */
 #include <Wire.h>
+#define LED_PIN 13
+
 void setupWire()
 {
   Wire.begin(8);                // join i2c bus with address #8
   Wire.onReceive(parseCMD); // register event
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
 }
+
 // function that executes whenever data is received from master
 // this function is registered as an event, see setup()
 void printCMD(int howMany)
@@ -15,47 +20,54 @@ void printCMD(int howMany)
 	char sentArr[Wire.available()];
 	byte i = 0;
   
-	while (1 < Wire.available()) {
+	while (Wire.available() > 0) {
 		char c = Wire.read(); // receive byte as a character
-		sentArr[i] = c;
-		i++;
+		sentArr[i++] = c;
   	} 
   
 	for (int j = 0; j < i; j++)
 	{
 		Serial.print(sentArr[j]);
 	}
-  Serial.print("\n");
 }
 
 void parseCMD(int num)
 {
   char* cmd = nextThreeBytes();
-  if(cmd != "RBT")
+  Serial.print("cmd: ");
+  Serial.println(cmd);
+  if(strcmp(cmd,"RBT") != 0)
     return;
   
-  while(cmd != "END" && Wire.available() > 2)
+  while(strcmp(cmd, "END") && Wire.available() > 0)
   {
     cmd = nextThreeBytes();
-    if(cmd == "RUN")
+    Serial.print("cmd: ");
+    Serial.println(cmd);
+  
+    if(strcmp(cmd, "RUN") == 0)
     {
       cmd = nextThreeBytes();
       do
       {
-        if(cmd == "LFT")
+        if(strcmp(cmd, "LFT") == 0)
         {
-          turn(false, ROLL, PITCH_POSITIVE, PITCH_NEGATIVE, PIVOT_PITCH_POSITIVE, PIVOT_PITCH_NEGATIVE, WAIT);
-        }
-        else if(cmd == "RGT")
+          //servo loop for turn left
+	  turn(false, ROLL, PITCH_POSITIVE, PITCH_NEGATIVE, PIVOT_PITCH_POSITIVE, PIVOT_PITCH_NEGATIVE, WAIT);
+       }
+        else if(strcmp(cmd, "RGT") == 0) 
         {
+          //servo loop for turn right
           turn(true, ROLL, PITCH_POSITIVE, PITCH_NEGATIVE, PIVOT_PITCH_POSITIVE, PIVOT_PITCH_NEGATIVE, WAIT);
         }
-        else if(cmd == "FWD")
+        else if(strcmp(cmd, "FWD") == 0) 
         {
+          //servo loop for forwards
           forward(true, ROLL, PITCH_POSITIVE, PITCH_NEGATIVE, WAIT);
         }       
-        else if(cmd == "BWD")
+        else if(strcmp(cmd, "BWD") == 0 )
         {
+          //servo loop for back
           backward(true, ROLL, PITCH_POSITIVE, PITCH_NEGATIVE, WAIT);
         }
         else
@@ -64,24 +76,36 @@ void parseCMD(int num)
         }
       }while(true);
     }
+    else if(strcmp(cmd, "LED") == 0)
+    {
+	cmd = nextThreeBytes();
+        
+        if(strcmp(cmd, "ONN") == 0)
+	{
+		digitalWrite(LED_PIN, HIGH);
+	}
+	else if(strcmp(cmd,"OFF") == 0)
+	{
+		digitalWrite(LED_PIN, LOW);
+	}
+    }
   }
 }
 
 char* nextThreeBytes()
 {
-  if(Wire.available() < 2)
+  if(Wire.available() < 3)
     return NULL;
   
-  char cmd[3];
+  char* cmd = (char*)calloc(4, sizeof(char));
   for(int i = 0; i < 3; i++)
   {
     cmd[i] = Wire.read();
-    Serial.print(cmd[i]);
   }
+  
   // read the space
   if(Wire.available() > 0)
-  {
-    Wire.read();
-  }
+    Wire.read();	
+  
   return cmd;
 }
